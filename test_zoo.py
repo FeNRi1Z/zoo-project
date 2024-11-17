2f3c8a5ee438b75de6c1e27604ef721a4423ced6
Phakawat Rattanasopa <dewmagic2013@gmail.com>
2024-11-17 16:13:39 +0700

    Added Test Cases and Fixed Zoo class

--- test_zoo.py
+++ test_zoo.py
@@ -4,11 +4,34 @@ from zoo import Zoo
 class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
-
+        
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    
+    #invalid age
+    def test_invalid_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid age")
+    
+    #child ticket price
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+    
+    #teen ticket price
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    
+    #adult ticket price
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    #old ticket price
+    def test_old_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,15 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
-            return 50
-        elif 13 <= age < 20:
-            return 100
-        elif 21 < age <= 60:
-            return 150
-        elif age >= 60:
-            return 100
+        try:
+            if 0 <= age <= 12:
+                return 50
+            elif 13 <= age <= 20:
+                return 100
+            elif 21 <= age <= 60:
+                return 150
+            elif age > 60:
+                return 100
+            else:
+                return "Invalid age"
+        except:
+            return "Invalid age"
